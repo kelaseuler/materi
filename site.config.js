3353617358f19172ab2c69e0c5a714084e61792f
kelaseuler <165623436+kelaseuler@users.noreply.github.com>
2024-04-01 15:52:24 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -26,6 +26,7 @@ const CONFIG = {
   link: "https://materi-kelaseulers-projects.vercel.app/",
   since: 2024, // If leave this empty, current year will be used.
   lang: "id-ID", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
